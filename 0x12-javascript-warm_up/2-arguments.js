f2e79c23480940f805451f62e27b3b525285163a
chukwu1455 <tuk2chuks@gmail.com>
2023-08-14 23:02:10 +0000

    no argument

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
-
-const myVar = 'Javascript is amazing';
-console.log(myVar);
+if (process.argv.length < 3) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
