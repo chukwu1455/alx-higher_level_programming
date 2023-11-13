d6b6dddda0ddbc2fefe8b7399a1d1db9110efe20
chukwu1455 <tuk2chuks@gmail.com>
2023-11-13 06:48:12 -0800

    3 language

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,8 @@
 #!/usr/bin/node
-function printMessage (...args) {
-  if (args.length === 0) {
-    console.log('No argument');
-  } else if (args.length === 1) {
-    console.log('Argument found');
-  } else {
-    console.log('Arguments found');
-  }
+if (process.argv.length < 3) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
-// Test the function
-printMessage(); // No argument
-printMessage('Hello'); // Argument found
-printMessage('Hello', 'World'); // Arguments found
